b13b6706c776a3f81053a22a6f703a444b242d75
AndyTakker <andrei.kolesov@gmail.com>
2026-01-30 17:25:31 +0300

    Настройка портов и прерываний помещена в метод init(). Добавлен вариант использования библиотеки gpio

--- examples/EncoderSample.cpp
+++ examples/EncoderSample.cpp
@@ -1,75 +1,54 @@
+// #define USE_GPIO_LIB
 #include "Encoder.h"
 #include <debug.h>
 
-// Подключаем энкодер к пинам PC0 и PC1. Для смены направления сменить порядок пинов
-Encoder encoder({GPIOC, GPIO_Pin_1, EXTI_Line1}, {GPIOC, GPIO_Pin_0, EXTI_Line0});
-
-//==============================================================================
-// Настройка прерываний для работы энекодера на PC0 и PC1
-//------------------------------------------------------------------------------
-void EXTI_INIT(void) {
-  GPIO_InitTypeDef GPIO_InitStructure = {0};
-  EXTI_InitTypeDef EXTI_InitStructure = {0};
-  NVIC_InitTypeDef NVIC_InitStructure = {0};
-
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOC, ENABLE);
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1; // PC0, PC1 на вход
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-  // GPIOC ----> EXTI_Line0
-  GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource0);
-  EXTI_InitStructure.EXTI_Line = EXTI_Line0;
-  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
-  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-  EXTI_Init(&EXTI_InitStructure);
-
-  // GPIOC ----> EXTI_Line1
-  GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource1);
-  EXTI_InitStructure.EXTI_Line = EXTI_Line1;
-  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
-  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-  EXTI_Init(&EXTI_InitStructure);
-
-  NVIC_InitStructure.NVIC_IRQChannel = EXTI7_0_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
-}
+// Подключаем энкодер. Для смены направления сменить порядок пинов
+#ifdef USE_GPIO_LIB
+#include <gpio.h>
+Encoder encoder(PC4, PC3);
+#else
+Encoder encoder({GPIOC, GPIO_Pin_4}, {GPIOC, GPIO_Pin_3});
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-void EXTI7_0_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+
 //==============================================================================
 // При срабатывании контактов энкодера вызывается функция и вычисляет новое
 // положение энкодера.
-// Сюда же можно добавить обрабочики прерываний с других входов.
+// Сюда же можно добавить обработчики прерываний с других входов.
 //------------------------------------------------------------------------------
-void EXTI7_0_IRQHandler(void) {
-  encoder.refresh();  // Вызываем функцию обработки энкодера.
+#ifdef USE_GPIO_LIB
+PIN_INT_ISR {
+  encoder.refresh(); // Вызываем функцию обработки энкодера.
 }
+#else
+void EXTI7_0_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void EXTI7_0_IRQHandler(void) {
+  encoder.refresh(); // Вызываем функцию обработки энкодера.
+}
+#endif
+
 #ifdef __cplusplus
 }
 #endif
 
 int main() {
   SystemCoreClockUpdate();
+
+#ifdef LOG_ENABLE
   Delay_Init();
-  Delay_Ms(4000); // Ждем, пока раздуплится терминал в VSCode
   USART_Printf_Init(115200);
   printf("SystemClk: %lu\r\n", SystemCoreClock);      // Для посмотреть частоту процесора (48мГц)
   printf("   ChipID: %08lx\r\n", DBGMCU_GetCHIPID()); // Для посмотреть ID чипа, от нефиг делать
+#endif
 
-  EXTI_INIT(); // Настройка прерываний
-  encoder.init();
+  encoder.init(); // Настройка портов энкодера и прерываний и пр.
   while (1) {
+#ifdef LOG_ENABLE
     printf("Encoder: %ld\r\n", encoder.encPos);
     Delay_Ms(500);
+#endif
   }
 }
--- src/Encoder.h
+++ src/Encoder.h
@@ -1,23 +1,57 @@
-#ifndef ENCODER_H
-#define ENCODER_H
+//==============================================================================
+// Библиотека для работы с энкодером на CH32V003.
+// Минималистический вариант, только вращения. Никак не учитывает нажатие кнопки
+// (т.к. от просто кнопка) и не отслеживает скорость вращения (это несложно 
+// реализовать в прикладном коде). 
+// Реализована в двух вариантах:
+// 1. С использованием "стандартных" функций Noneos-sdk (в стиле STM32)
+// 2. С использованием библиотеки gpio.h
+// Выбор варианта осуществляется флагом USE_GPIO_LIB.
+// Преимущества использования библиотеки gpio:
+// - код получается немного меньше. Экономия flash около 300 байт.
+// - позволяет обращаться к пинам по алиасам PD0, PD1, PD2, PD3, PD4, PD5, PD6 и т.д.
+//   что упрощает конфигурирование пинов через #define.
+// К минусам стоит отнести то, что вся gpio построена на дефайнах и нагладностью
+// кода не отличается. Но включив build_flag = -save-temps можно потом посмотреть
+// в .ii файлах фактически получившийся код.
+//
+// Для использования библиотеки энкодера нужно:
+// 1. Создать объект encoder
+// 2. Создать обрабочик прерывания EXTI7_0_IRQHandler и в нем вызвать encoder.refresh()
+//    Если обработчик уже есть, то просто добавить в него вызов encoder.refresh()
+// 3. В начале main вызвать encoder.init()
+// 4. Положение энкодера доступно через encoder.encPos
+// Смотри пример в examples/EncoderSample.cpp
+//------------------------------------------------------------------------------
+#pragma once
+// #define USE_GPIO_LIB // Подключить библиотеку GPIO через флаг USE_GPIO_LIB в platformio.ini
 #include <ch32v00x.h>
 
+#ifndef USE_GPIO_LIB
 typedef struct {
-    GPIO_TypeDef *port;
-    uint16_t pin;
-    uint32_t extiLine;
+  GPIO_TypeDef *port;
+  uint16_t pin;
 } ExtiCfg;
+#endif
 
 class Encoder {
-private: 
-    ExtiCfg _pinDt;
-    ExtiCfg _pinClk;
+  private:
+#ifdef USE_GPIO_LIB
+  uint8_t _pinDt;
+  uint8_t _pinClk;  
+#else
+  ExtiCfg _pinDt;
+  ExtiCfg _pinClk;
+  void portSetting(ExtiCfg pin);
+#endif
 
-public: 
-    volatile int32_t encPos = 0;     // Положение энкодера. За один полный оборот получаем 80 импульсов
-    Encoder(ExtiCfg pinDT, ExtiCfg pinCLK);
-    void init(void);
-    void refresh();
+  public:
+  volatile int32_t encPos = 0; // Положение энкодера
+#ifdef USE_GPIO_LIB
+  Encoder(uint8_t pinDT, uint8_t pinCLK);
+#else  
+  Encoder(ExtiCfg pinDT, ExtiCfg pinCLK);
+#endif  
+  void init(void);
+  void refresh();
 };
-
-#endif // ENCODER_H
